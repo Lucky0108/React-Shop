076456cfcfe5749b5529da53ac7af334bcb174f4
Lucky0108 <raolakshayyadav02@gmail.com>
2020-11-01 20:47:09 +0530

    Update Cart.jsx

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -5,9 +5,9 @@ function Cart() {
     return (
         <>
             <div className="fullCart">
-            <div class="card" style={{width: "85%",height:"50%"}}>
-            <div class="card-body">
-                <h5 class="card-title font-weight-bold">My Cart</h5>
+            <div className="card" style={{width: "85%",height:"50%"}}>
+            <div className="card-body">
+                <h5 className="card-title font-weight-bold">My Cart</h5>
                 <div className="text-center">
                 <img src='https://rukminim1.flixcart.com/www/800/800/promos/16/05/2019/d438a32e-765a-4d8b-b4a6-520b560971e8.png?q=90' className="img-fluid" style={{height:"162px"}} alt="" />
                 <h6 style={{fontSize:"18px"}}>
