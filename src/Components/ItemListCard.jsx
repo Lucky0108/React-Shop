bf116be6181ebf8e27370f899de6bf6cbb2650f4
Lucky0108 <raolakshayyadav02@gmail.com>
2020-11-01 20:47:16 +0530

    Update ItemListCard.jsx

--- src/Components/ItemListCard.jsx
+++ src/Components/ItemListCard.jsx
@@ -10,8 +10,8 @@ function Card(props) {
                 <h5 className="card-title">{props.title} </h5>
                 <p className="card-text">{props.detail}</p>
                 <div className="d-flex justify-content-between">
-                <NavLink to={props.Callref} className="btn btn-outline-primary">Add to Wishlist</NavLink>
-                <NavLink to={props.Callref} className="btn btn-outline-success">Buy Now</NavLink>
+                <NavLink to={props.Callref} onClick={()=>{alert(`${props.title} is succesfully added to Wishlist`)}} className="btn btn-outline-primary">Add to Wishlist</NavLink>
+                <NavLink to={props.Callref} onClick={()=>{alert(`Congratulations ! ${props.title} is succesfully Purchased!`)}} className="btn btn-outline-success">Buy Now</NavLink>
                 </div>
             </div>
         </div>
