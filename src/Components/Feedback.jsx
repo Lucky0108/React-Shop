6bcba73b1751d86d5d6d0caa9270c237795055bd
Lucky0108 <raolakshayyadav02@gmail.com>
2020-11-01 20:47:15 +0530

    Update Feedback.jsx

--- src/Components/Feedback.jsx
+++ src/Components/Feedback.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Feedback() {
     document.title = "My Feedback";
+    const [feed,setFeed] = useState({
+        name: "Lakshay",
+        feedback: ""
+    });
+
+    const inputEvent = (e) => {
+        const {name,value} = e.target;
+        setFeed((preVal)=>{
+            return {
+                ...preVal,
+                [name]: value
+            }
+        })
+    }
+
+    const fSubmit = (e) => {
+        e.preventDefault();
+        console.log(feed)
+        alert(`Hey ${feed.name}, Your Feedback '${feed.feedback}' is much appreciated.`)
+    }
+
     return (
         <>
             <div className="fullFeedback">
@@ -9,15 +30,15 @@ function Feedback() {
             <div className="container">
             <div className="card-body">
                 <h3 className="card-title text-center">Feedback Portal</h3>
-                <small class="form-text text-center text-muted">Your Feedback and Suggestions helps us to improve.</small>
-                <form>
+                <small className="form-text text-center text-muted">Your Feedback and Suggestions helps us to improve.</small>
+                <form onSubmit={fSubmit}>
                 <div className="form-group">
-                    <label for="name">Name</label>
-                    <input type="text" value="Lakshay" className="form-control disableInput" id="name" name="name" disabled />
+                    <label htmlFor="name">Name</label>
+                    <input type="text" value={feed.name} onChange={inputEvent} className="form-control disableInput" id="name" name="name" disabled />
                 </div>
                 <div className="form-group">
-                    <label for="query">Your Feedback</label>
-                    <textarea className="form-control" id="feedbackMessage" rows="3"></textarea>
+                    <label htmlFor="query">Your Feedback</label>
+                    <textarea className="form-control" id="feedbackMessage" onChange={inputEvent} value={feed.feedback} name="feedback" rows="3" required></textarea>
                 </div>
                 <button type="submit" className="btn btn-outline-success">Submit</button>
                 </form>
