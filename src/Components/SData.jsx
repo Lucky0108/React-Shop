43d7df84217b8da5d6b69423615db129b246a17c
Lucky0108 <raolakshayyadav02@gmail.com>
2020-11-01 21:27:49 +0530

    Update SData.jsx

--- src/Components/SData.jsx
+++ src/Components/SData.jsx
@@ -33,7 +33,7 @@ const Sdata = [
         title: "Books",
         src: "https://source.unsplash.com/600x400/?Books",
         detail: "Some quick example text to build on the card title and make up the bulk of the card's content.",
-        refCall: "/books"
+        refCall: "/"
     },
     {
         title: "television",
